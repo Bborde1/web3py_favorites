814498494e19e1d888af268d1d6dad81952c5401
Bborde1 <Brandon.Borde@gmail.com>
2025-02-23 00:09:57 -0500

    updated contrat deploy script with encryption

--- deploy_favorites.py
+++ deploy_favorites.py
@@ -2,13 +2,15 @@ from vyper import compile_code
 from web3 import Web3
 from dotenv import load_dotenv
 import os
+from encrypt_key import KEYSTORE_PATH
+import getpass
+from eth_account import Account
 
 """This project requries that a .env file in the directory with the variable below"""
 
 load_dotenv()
 RPC_URL = os.getenv("RPC_URL")
 MY_ADDRESS = os.getenv("MY_ADDRESS")
-MY_KEY = os.getenv("MY_KEY")
 
 
 def main():
@@ -31,11 +33,26 @@ def main():
     transaction = favorites_contract.constructor().build_transaction(
         {"nonce": nonce, "from": MY_ADDRESS, "gasPrice": w3.eth.gas_price}
     )
+    my_key = decrypt_key()
     signed_transaction = w3.eth.account.sign_transaction(
-        transaction, private_key=MY_KEY
+        transaction, private_key=my_key
     )
     print(signed_transaction)
     # breakpoint()
+
+    tx_hash = w3.eth.send_raw_transaction(signed_transaction.raw_transaction)
+    print(f"My Tx hash is {tx_hash}")
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
+
+
+def decrypt_key():
+    with open(KEYSTORE_PATH) as fp:
+        encrypted_account = fp.read()
+        password = getpass.getpass("Enter your password: \n")
+        key = Account.decrypt(encrypted_account, password)
+        print("Decrypted Key!")
+        return key
 
 
 if __name__ == "__main__":
